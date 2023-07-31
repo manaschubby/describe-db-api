b40742cbc71a8bf99148f239b8fca7bfac586e1d
manas <manaschubby@gmail.com>
2023-07-31 13:55:23 +0530

    add: PostgreSQL Endpoint

--- main.py
+++ main.py
@@ -2,14 +2,32 @@ import mysql.connector
 import flask
 from flask import request, jsonify
 from mysql_schema import get_mysql_schema
+from postgresql_schema import get_psql_schema
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-# Route for MySQL Schema
-@app.route('/mysql', methods=['GET'])
-def getSchema():
-    return get_mysql_schema(request.args)
 
-if __name__ == '__main__':
+# Route for MySQL Schema
+@app.route("/mysql", methods=["GET"])
+def getMysqlSchema():
+    return get_mysql_schema(request.get_json())
+
+
+# Route for PostgreSQL Schema
+@app.route("/psql", methods=["GET"])
+def getPsqlSchema():
+    return get_psql_schema(request.get_json())
+
+
+@app.route("/", methods=["GET"])
+def home():
+    print(request.get_json())
+    return "<h1>Schema Generator</h1><p>This site is a prototype API for generating schema for MySQL and PostgreSQL databases.</p>"
+
+
+#  Run the app using the following command:
+#  python main.py
+
+if __name__ == "__main__":
     app.run()
--- postgresql_schema.py
+++ postgresql_schema.py
@@ -2,11 +2,57 @@ import psycopg2
 from flask import jsonify
 from mysql_schema import validate_mysql_req
 
+
 def get_psql_schema(data):
-    pass
+    if validate_psql_req(data) == False:
+        return jsonify({"error": "Invalid Request"})
+    try:
+        psql_conn = psycopg2.connect(
+            host=data["host"],
+            port=data["port"],
+            user=data["user"],
+            password=data["password"],
+            database=data["database"],
+        )
+        psql_cursor = psql_conn.cursor()
+
+        # Using a parameterized query to retrieve table names
+        psql_cursor.execute(
+            "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'"
+        )
+        tables = psql_cursor.fetchall()
+
+        json_tables = []
+        for table in tables:
+            # Using a parameterized query to retrieve column information for each table
+            psql_cursor.execute(
+                "SELECT column_name, data_type, is_nullable, column_default FROM information_schema.columns WHERE table_name = %s",
+                (table[0],),
+            )
+            columns = psql_cursor.fetchall()
+            json_columns = []
+            for column in columns:
+                json_columns.append(
+                    {
+                        "Field": column[0],
+                        "Type": column[1],
+                        "Null": column[2],
+                        "Default": column[3],
+                    }
+                )
+            json_tables.append({"Table": table[0], "Columns": json_columns})
+        print(json_tables)
+        psql_cursor.close()
+        psql_conn.close()
+        return jsonify({"success": "true", "data": json_tables})
+
+    except Exception as e:
+        print(e)
+        return jsonify({"error": "Invalid Request"})
+
 
 def validate_psql_req(data):
-    if data['port'] == '':
-        data['port'] = 5432
+    if data["port"] == "":
+        data["port"] = 5432
     # Rest of the validation is same as mysql
     return validate_mysql_req(data)
